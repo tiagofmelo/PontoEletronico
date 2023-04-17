c3e4c9b184cb286a68e8e1cbd7e11f756c92699d
Tiago Melo <Tiago.Melo@br.ey.com>
2023-04-17 19:27:24 -0300

    Add project files.

---
+++ PontoEletronico/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PontoEletronico.Models;
+
+namespace PontoEletronico.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return RedirectToAction("Login", "Home");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = "Erro ao realizar login \n" + erro.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ PontoEletronico/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PontoEletronico.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Usuário obrigatório")]
+        public string userID { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória")]
+        public string accessKey { get; set; }
+
+        public string grantType { get => "password"; }
+    }
+}
