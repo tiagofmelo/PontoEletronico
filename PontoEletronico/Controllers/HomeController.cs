585dc07ee05436d3615e77cecc4cef223c9389f9
Tiago Melo <Tiago.Melo@br.ey.com>
2023-04-18 16:12:15 -0300

    Layout Tela de ponto

--- PontoEletronico/Controllers/HomeController.cs
+++ PontoEletronico/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PontoEletronico.Models;
 
 namespace PontoEletronico.Controllers
 {
     public class HomeController : Controller
     {
+        List<TimesheetModel> timesheetModels;
+
         public ActionResult Index()
         {
-            return View();
+            timesheetModels = new List<TimesheetModel>()
+            {
+                new TimesheetModel()
+                {
+                    id= 1,
+                    start= DateTime.Now,
+                    startLunch= DateTime.Now,
+                    endLunch= DateTime.Now,
+                    end = DateTime.Now
+                }
+            };
+
+            return View(timesheetModels);
         }
 
         public ActionResult Login()
---
+++ PontoEletronico/Models/TimesheetModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace PontoEletronico.Models
+{
+    public class TimesheetModel
+    {
+        public int id { get; set; }
+        public DateTime start { get; set; }
+        public DateTime startLunch { get; set; }
+        public DateTime endLunch { get; set; }
+        public DateTime end { get; set; }
+        public string date
+        {
+            get
+            {
+                if (start.Ticks == 0)
+                    return string.Empty;
+
+                return start.ToString("d");
+            }
+        }
+        public string totalTime
+        {
+            get
+            {
+                
+                TimeSpan totalTime;
+
+                if (start.Ticks == 0)
+                    return string.Empty;
+
+                if (end.Ticks == 0)
+                {
+                    totalTime = DateTime.Now.Subtract(start);
+                }
+                else
+                {
+                    totalTime = end.Subtract(start);
+                }
+
+                return String.Format("{0:t}", totalTime);
+            }
+        }
+    }
+
+    public class Pagination
+    {
+        public TimesheetModel[] items { get; set; }
+        public int count { get; set; }
+        public int currentPage { get; set; }
+        public int pageSize { get; set; }
+        public int totalPages { get; set; }
+        public string nextPage { get; set; }
+        public string previousPage { get; set; }
+    }
+}
